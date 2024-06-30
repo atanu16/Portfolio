9a9f44a07cb27ece0ef8a2e773b71982f393a8ad
atanu16 <atanumax2@gmail.com>
2024-07-01 03:21:38 +0530

    Blog ui added

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -75,7 +75,7 @@ const Blog = () => {
             <h2 className="mb-6 text-5xl font-extrabold lg:text-6xl font-montserrat gradient-text">
               Blog
             </h2>
-            <p className="font-medium text-xl lg:text-2xl text-accent max-w-[960px] mx-auto">
+            <p className="font-medium text-xl lg:text-xl text-accent max-w-[960px] mx-auto">
               Welcome to my blog! Here you will find the latest updates and articles.
             </p>
           </header>
